21875d71f1b75d99afed91980dc871084f3366c6
Nam Nguyen <nxnam714@gmail.com>
2024-07-31 20:56:21 +0700

    init

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='testWheel',  # Name of your package
+    version='1.0',     # Version number
+    packages=['testWheel'],  # Names of your packages (folder containing .py files)
+)
