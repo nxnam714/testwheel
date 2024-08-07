ae84041181766a5042cafc4458d7ec376641f963
Nam Nguyen <nxnam714@gmail.com>
2024-08-07 20:39:13 +0700

    bdist wheel practice

--- setup.py
+++ setup.py
@@ -1,7 +1,42 @@
-from setuptools import setup
+import logging
+import setuptools.command.egg_info as egg_info
+from setuptools import setup, find_packages, find_namespace_packages
+from distutils import log
+import setuptools
 
+
+
+logging.basicConfig(
+    format='%(module)-15s %(lineno)-8s %(message)s',
+    style="%",
+    level=logging.DEBUG
+)
+
+setattr(setuptools, "logging", logging)
+logger = logging.getLogger(__name__)
+log.info = logger.info
+
+public = False
+if public:
+    o_manifest_maker = egg_info.manifest_maker
+
+    class manifest_maker(o_manifest_maker):
+        template = "MANIFEST.pub"
+
+    egg_info.manifest_maker = manifest_maker
+    exclude = ("*.internal", "*.internal.*")
+else:
+    exclude = ()
+
+packages = find_namespace_packages(
+    where=".",
+    include=("testWheel", "testWheel.*", ),
+    exclude=exclude,
+)
+print(packages)
 setup(
     name='testWheel',  # Name of your package
     version='1.0',     # Version number
-    packages=['testWheel'],  # Names of your packages (folder containing .py files)
+    packages=packages,
+    include_package_data=True,
 )
